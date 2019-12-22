e9d0600d67198ab8243b12b6e56eda9ada01c3df
Omer Avrech <omera@ceragon.com>
2019-12-23 00:09:30 +0200

    runtime bug fixed

--- src/Redux/weather.action.js
+++ src/Redux/weather.action.js
@@ -1,4 +1,4 @@
-import axios from '../API/AccuWeather'
+import AccuWeatherAPI from './../API/AccuWeather';
 
 export const FETCH_CITY_INFO = 'FETCH_CITY_INFO';
 export const FETCH_CITY_DATA = 'FETCH_CITY_DATA';
@@ -6,7 +6,7 @@ export const FETCH_CITY_DATA = 'FETCH_CITY_DATA';
 export const fetch_city_info = (cityName) => {
     return {
         type: FETCH_CITY_INFO,
-        payload: axios.get(`/locations/v1/cities/search`, {params: {q: cityName }})
+        payload: AccuWeatherAPI.get(`/locations/v1/cities/search`, {params: {q: cityName }})
     }
 }
 
@@ -14,8 +14,8 @@ export const fetch_forecast = (cityKey) => {
     return {
         type: FETCH_CITY_DATA,
         payload: Promise.all([
-            axios.get(`/currentconditions/v1/${cityKey}`, {params: {details: true }}),
-            axios.get(`/forecasts/v1/daily/5day/${cityKey}`, {params: {metric: true }})
+            AccuWeatherAPI.get(`/currentconditions/v1/${cityKey}`, {params: {details: true }}),
+            AccuWeatherAPI.get(`/forecasts/v1/daily/5day/${cityKey}`, {params: {metric: true }})
         ])
     }
 }
